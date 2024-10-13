441ce09e6ba4f9f3d5811ab404592b80864f3729
Dovalkin <tranquangtuananh2801@gmail.com>
2024-10-14 02:45:47 +1000

    Update PText.js

--- my_folio/src/components/PText.js
+++ my_folio/src/components/PText.js
@@ -2,14 +2,14 @@ import React from 'react';
 import styled from 'styled-components';
 
 const PStyle = styled.div`
-  max-width: 500px;
+  width: 100%;
   justify-content: center;
   align-items: center;
   font-size: 2.3rem;
   line-height: 2.2rem;
+  margin: 0 auto;
   @media only screen and (max-width: 1080px) {
     font-size: 2.2rem;
-    margin: 0 auto;
   }
   @media only screen and (max-width: 768px) {
     font-size: 1.8rem;
