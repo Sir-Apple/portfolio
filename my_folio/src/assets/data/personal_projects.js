d8dc1b7da27cced868a619fab1bc1d6d7fdc2ffe
Dovalkin <tranquangtuananh2801@gmail.com>
2024-10-13 22:42:17 +1000

    Update personal_projects.js

--- my_folio/src/assets/data/personal_projects.js
+++ my_folio/src/assets/data/personal_projects.js
@@ -23,7 +23,7 @@ const projects = [
     desc:
       'My previous version of my portfolio, made with HTML, CSS, and JavaScript',
     img: OldPortfolioImg,
-    webLink: 'sir-apple-github-io.vercel.app/',
+    webLink: 'https://sir-apple-github-io.vercel.app/',
     githubLink: 'https://github.com/Sir-Apple/Sir-Apple.github.io',
   },
   {
